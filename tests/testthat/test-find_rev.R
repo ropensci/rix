3525911d8ec11e9ca7c989e316b4871a049c2ade
Bruno Rodrigues <bruno.rodrigues@mesr.etat.lu>
2023-08-10 22:47:23 +0200

    added possibility to return nixpkgs revision in find_rev, still need to test and inflate

--- tests/testthat/test-find_rev.R
+++ tests/testthat/test-find_rev.R
@@ -6,6 +6,11 @@ testthat::expect_equal(
           )
 
 testthat::expect_equal(
+            find_rev("8ad5e8132c5dcf977e308e7bf5517cc6cc0bf7d8"),
+            "8ad5e8132c5dcf977e308e7bf5517cc6cc0bf7d8"
+          )
+
+testthat::expect_equal(
             get_sri_hash_deps("https://github.com/rap4all/housing/",
                          "fusen",
                         "1c860959310b80e67c41f7bbdc3e84cef00df18e"),
