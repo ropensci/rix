077dcd596b80010d26dc9e7b87698c5c448d74d1
Bruno Rodrigues <b-rodrigues@localhost.localdomain>
2024-05-21 22:11:00 +0200

    skip this test on windows as sed is not available unless running on wsl

--- tests/testthat/test-ga_cachix.R
+++ tests/testthat/test-ga_cachix.R
@@ -1,5 +1,7 @@
 testthat::test_that("Snapshot test of ga_cachix()", {
 
+  skip_if(Sys.info()["sysname"] == "Windows")
+
   testthat::announce_snapshot_file("ga_cachix/cachix_dev_env.yaml")
 
   testthat::expect_snapshot_file(
