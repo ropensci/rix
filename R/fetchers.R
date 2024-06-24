4bb9afe8f71cf688c797f5ee798b63c7d267807c
Bruno Rodrigues <bruno@brodrigues.co>
2024-06-24 13:57:33 +0200

    added trimws for good measure

--- R/fetchers.R
+++ R/fetchers.R
@@ -130,7 +130,7 @@ get_imports <- function(path) {
   # Remove version of R that may be listed in 'Depends'
   output <- Filter(function(x) !grepl("R \\(.*\\)", x), output)
 
-  output <- gsub("\\(.*?\\)", "", output)
+  output <- trimws(gsub("\\(.*?\\)", "", output))
 
   output <- remove_base(unique(output))
 
