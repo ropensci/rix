a6db4ec1cd050b81171e8921292ab3a768de373b
Bruno Rodrigues <bruno@brodrigues.co>
2024-07-11 11:04:26 +0200

    update action's permissions

--- tests/testthat/test-ga_cachix.R
+++ tests/testthat/test-ga_cachix.R
@@ -1,9 +1,6 @@
-# for development on Windows
-ga_cachix(cache_name = "rstats-on-nix", path = "default.nix")
-
 testthat::test_that("Snapshot test of ga_cachix()", {
-  skip_if(Sys.info()["sysname"] == "Windows")
-  
+  skip_if(Sys.info()["sysname"] != "Linux")
+
   testthat::announce_snapshot_file("ga_cachix/cachix_dev_env.yaml")
 
   testthat::expect_snapshot_file(
