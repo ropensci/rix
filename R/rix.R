4d9005d348999816db5d23366fa41aa2827c7386
Bruno Rodrigues <bruno@brodrigues.co>
2024-09-12 15:43:18 +0200

    - Gitlab packages can now be installed as well

--- R/rix.R
+++ R/rix.R
@@ -75,7 +75,7 @@
 #'   argument to `"other"`. We recommend reading the
 #'   `vignette("e-interactive-use")` for more details.
 #'
-#'   Packages to install from Github must be provided in a list of 3 elements:
+#'   Packages to install from Github or Gitlab must be provided in a list of 3 elements:
 #'   "package_name", "repo_url" and "commit". To install several packages,
 #'   provide a list of lists of these 3 elements, one per package to install. It
 #'   is also possible to install old versions of packages by specifying a
@@ -89,9 +89,9 @@
 #'   the Nix revision closest to that date, by setting `r_ver = "3.1.0"`, which
 #'   was the version of R current at the time. This ensures that Nix builds a
 #'   completely coherent environment. For security purposes, users that wish to
-#'   install packages from Github or from the CRAN archives must provide a
+#'   install packages from Github/Gitlab or from the CRAN archives must provide a
 #'   security hash for each package. `{rix}` automatically precomputes this hash
-#'   for the source directory of R packages from GitHub or from the CRAN
+#'   for the source directory of R packages from GitHub/Gitlab or from the CRAN
 #'   archives, to make sure the expected trusted sources that match the
 #'   precomputed hashes in the `default.nix` are downloaded. If Nix is
 #'   available, then the hash will be computed on the user's machine, however,
@@ -169,7 +169,7 @@ rix <- function(r_ver = "latest",
     warning(
       "You chose 'bleeding_edge' or 'frozen_edge' as the value for
 `r_ver`. Please read the vignette
-https://ropensci.github.io/rix/articles/z-advanced-topic-understanding-the-rpackages-set-release-cycle-and-using-bleeding-edge-packages.html
+https://ropensci.github.io/rix/articles/z-bleeding_edge.html
 before continuing."
     )
   }
