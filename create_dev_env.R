b5fe46d722669d59c3859c4325351c6d7dc6df4c
Philipp Baumann <baumann-philipp@protonmail.com>
2024-09-16 19:35:06 +0200

    add docopt for precommit nix R hooks

--- create_dev_env.R
+++ create_dev_env.R
@@ -5,7 +5,7 @@ rix(
   r_pkgs = c(
     "devtools", "diffviewer", "fledge", "lintr", "styler",
     "codetools", "jsonlite", "httr", "sys", "testthat", "knitr",
-    "rmarkdown", "rhub"
+    "rmarkdown", "rhub", "docopt"
   ),
   system_pkgs = c("R", "glibcLocalesUtf8", "pandoc", "nix"),
   tex_pkgs = "scheme-small",
