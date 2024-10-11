926102f6f0b9f68c553ada70096ee1ac36c6a7de
Richard J. Acton <richardjacton@pm.me>
2024-10-11 22:46:28 +0100

    * remove the now unnecessary `renv_lock_pkgs()` function

--- R/renv_helpers.R
+++ R/renv_helpers.R
@@ -93,56 +93,6 @@ renv2nix <- function(
     eval(rix_call)
 }
 
-
-#' renv_lock_pkgs
-#'
-#' gets the names of all the packages in an renv.lock file
-#'
-#' This does NOT get the versions of those packages or handle transitive
-#' dependencies in the renv.lock file not directly used in the project.
-#' It simply returns a vector of all the package names.
-#'
-#' @param renv_lock_path location of the renv.lock file from which to get the
-#' list of packages, defaults to "renv.lock"
-#' @param exclude_other_sources exclude packages not from Repository sources
-#' such as those from github repositories, defaults to TRUE. These are likely
-#' to not yet be packaged in nix.
-#'
-#' @return a character vector of all the package names listed in the renv.lock
-#' file proved in renv_lock_path.
-#'
-#' @export
-#'
-#' @examples
-#'
-#' rix(r_pkgs = renv_lock_pkgs())
-#'
-renv_lock_pkgs <- function(
-        renv_lock_path = "renv.lock", exclude_other_sources = TRUE
-    ) {
-    renv_lock <- read_renv_lock(renv_lock_path = renv_lock_path)
-    repo_pkgs_lgl <- logical(length = length(renv_lock$Packages))
-    for (i in seq_along(renv_lock$Packages)) {
-        if (renv_lock$Packages[[i]]$Source == "Repository") {
-            repo_pkgs_lgl[i] <- TRUE
-        } else {
-            repo_pkgs_lgl[i] <- FALSE
-        }
-    }
-    if (any(!repo_pkgs_lgl)) {
-        # note
-        # When a development version of a package that might be available in
-        # in other versions in nix in installed from github this approach
-        # excludes such packages
-        warning(
-            "found packages with a Source other than Repository",
-            ", These will be excluded\nExcluding: ",
-            paste(names(renv_lock$Packages[!repo_pkgs_lgl]), collapse = ", ")
-        )
-    }
-    names(renv_lock$Packages[repo_pkgs_lgl])
-}
-
 #' renv_lock_r_ver
 #'
 #' @param renv_lock_path location of the renv.lock file from which to get the
