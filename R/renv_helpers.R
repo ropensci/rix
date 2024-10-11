803bfd73d4d151460c88b29c0ecb0ffca7089300
Richard J. Acton <richardjacton@pm.me>
2024-10-11 13:18:03 +0100

    factor out reading renv.lock

--- R/renv_helpers.R
+++ R/renv_helpers.R
@@ -1,3 +1,25 @@
+#' read_renv_lock
+#'
+#' Reads renv.lock if it exists and can be parsed as json.
+#'
+#' @param renv_lock_path location of the renv.lock file, defaults to "renv.lock"
+#'
+#' @return the result of reading renv.lock with [jsonlite::read_json]
+#'
+#' @importFrom jsonlite read_json
+read_renv_lock <- function(renv_lock_path = "renv.lock") {
+    if (!file.exists(renv_lock_path)) {
+        stop(renv_lock_path," does not exist!")
+    }
+    tryCatch(
+        renv_lock <- jsonlite::read_json(renv_lock_path),
+        error = function(e) {
+            stop("Error reading renv.lock file\n", e)
+        }
+    )
+    renv_lock
+}
+
 #' renv_lock_pkgs
 #'
 #' gets the names of all the packages in an renv.lock file
@@ -19,14 +41,7 @@
 #' rix(r_pkgs = renv_lock_pkgs())
 #'
 renv_lock_pkgs <- function(renv_lock_path = "renv.lock") {
-    if (!file.exists(renv_lock_path)) {
-        stop(renv_lock_path," does not exist!")
-    }
-    tryCatch(
-        renv_lock <- jsonlite::read_json(renv_lock_path),
-        error = function(e) {
-            stop("Error reading renv.lock file\n", e)
-        }
-    )
+    renv_lock <- read_renv_lock(renv_lock_path = renv_lock_path)
     names(renv_lock$Packages)
 }
+
