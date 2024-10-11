701eafa53a00de6cc1439e578a37e732fef8ba63
Richard J. Acton <richardjacton@pm.me>
2024-10-11 13:54:57 +0100

    * add simple non-standard repo filtering to `renv_lock_pkgs()` function
    ---
    
    adds the `exclude_other_sources` option to `renv_lock_pkgs()` and
    defaults it to TRUE.
    This excludes packages from being returned if the do not have
    "Repository" as their value for "Source" in renv.lock the and throws a
    warning listing the excluded packages.
    This should exclude packages from sources like github which are not
    presently like to be packaged in nix.

--- R/renv_helpers.R
+++ R/renv_helpers.R
@@ -30,6 +30,9 @@ read_renv_lock <- function(renv_lock_path = "renv.lock") {
 #'
 #' @param renv_lock_path location of the renv.lock file from which to get the
 #' list of packages, defaults to "renv.lock"
+#' @param exclude_other_sources exclude packages not from Repository sources
+#' such as those from github repositories, defaults to TRUE. These are likely
+#' to not yet be packaged in nix.
 #'
 #' @return a character vector of all the package names listed in the renv.lock
 #' file proved in renv_lock_path.
@@ -40,9 +43,30 @@ read_renv_lock <- function(renv_lock_path = "renv.lock") {
 #'
 #' rix(r_pkgs = renv_lock_pkgs())
 #'
-renv_lock_pkgs <- function(renv_lock_path = "renv.lock") {
+renv_lock_pkgs <- function(
+        renv_lock_path = "renv.lock", exclude_other_sources = TRUE
+    ) {
     renv_lock <- read_renv_lock(renv_lock_path = renv_lock_path)
-    names(renv_lock$Packages)
+    repo_pkgs_lgl <- logical(length = length(renv_lock$Packages))
+    for (i in seq_along(renv_lock$Packages)) {
+        if (renv_lock$Packages[[i]]$Source == "Repository") {
+            repo_pkgs_lgl[i] <- TRUE
+        } else {
+            repo_pkgs_lgl[i] <- FALSE
+        }
+    }
+    if (any(!repo_pkgs_lgl)) {
+        # note
+        # When a development version of a package that might be available in
+        # in other versions in nix in installed from github this approach
+        # excludes such packages
+        warning(
+            "found packages with a Source other than Repository",
+            ", These will be excluded\nExcluding: ",
+            paste(names(renv_lock$Packages[!repo_pkgs_lgl]), collapse = ", ")
+        )
+    }
+    names(renv_lock$Packages[repo_pkgs_lgl])
 }
 
 #' renv_lock_r_ver
