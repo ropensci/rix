eed8e9a033f79714306af747ff6a42fc46b7e16b
Richard J. Acton <richardjacton@pm.me>
2024-10-12 15:33:29 +0100

    * add `renv_remote_pkgs()` function, factors out generating `git_pkgs` information
    ---
    
    The `renv_remote_pkgs()` function takes a single renv.lock package entry
    as an argument, infers it's remote type and extracts the informaton
    necesary for `rix()`'s `git_pkgs` argument.
    
    This functionality was factored out of the `renv2nix()`, and the
    `method` argument added `renv2nix()` to allow for a future
    implementation of exact package version matching.

--- R/renv_helpers.R
+++ R/renv_helpers.R
@@ -20,82 +20,116 @@ read_renv_lock <- function(renv_lock_path = "renv.lock") {
     renv_lock
 }
 
+#' renv_remote_pkgs
+#'
+#' Construct a list to be passed in a list to the git_pkgs argument of [rix]
+#' The list returned contains the information necessary to have nix attempt to
+#' build the package from an external repository.
+#'
+#' @param renv_lock_pkg_info a the list representation of a single package
+#' entry from an renv.lock file.
+#' @param type the type of remote package, defaults to the RemoteType of the
+#' renv entry.
+#' currently supported types: 'github' 'gitlab'
+#' see [remotes](https://remotes.r-lib.org/) for more.
+#'
+#' @return a list with three elements named:
+#'  "package_name", "repo_url", "commit"
+#'
+#' @examples
+#' \dontrun{
+#' renv_remote_pkgs(read_renv_lock()$Packages$renv)
+#' }
+renv_remote_pkgs <- function(
+    renv_lock_pkg_info,
+    type = renv_lock_pkg_info$RemoteType
+) {
+    type <- match.arg(type, c(
+        "github", "gitlab"
+        # , "bitbucket", "git", "local", "svn", "url", "version", "cran", "bioc"
+    ))
+    pkg_info <- vector(mode = "list", length = 3)
+    names(pkg_info) <- c("package_name", "repo_url", "commit")
+    switch (type,
+        "github" = {
+            pkg_info[[1]] <- renv_lock_pkg_info$Package
+            pkg_info[[2]] <- paste0(
+                # RemoteHost is listed as api.github.com for some reason
+                "https://github.com/", renv_lock_pkg_info$RemoteUser, "/",
+                renv_lock_pkg_info$RemoteRepo
+            )
+            pkg_info[[3]] <- renv_lock_pkg_info$RemoteSha
+        },
+        "gitlab" = {
+            pkg_info[[1]] <- renv_lock_pkg_info$Package
+            pkg_info[[2]] <- paste0(
+                "https://", renv_lock_pkg_info$RemoteHost, "/",
+                renv_lock_pkg_info$RemoteUser, "/",
+                renv_lock_pkg_info$RemoteRepo
+            )
+            pkg_info[[3]] <- renv_lock_pkg_info$RemoteSha
+        }
+    )
+    pkg_info
+}
+
 #' renv2nix
 #'
 #' @param renv_lock_path location of the renv.lock file, defaults to "renv.lock"
 #' @param return_rix_call return the generated rix function call instead of
 #' evaluating it this is for debugging purposes, defaults to FALSE
-#' @param ... any other paramters to pass to [rix]
+#' @param ... any other parameters to pass to [rix]
 #'
 #' @return nothing side effects only
 #' @export
 #'
 renv2nix <- function(
-    renv_lock_path = "renv.lock", return_rix_call = FALSE, ...
-
+    renv_lock_path = "renv.lock",
+    return_rix_call = FALSE,
+    method = "fast",
+    ...
 ) {
+    method <- match.arg(method, c("fast", "accurate"))
     renv_lock <- read_renv_lock(renv_lock_path = renv_lock_path)
-    repo_pkgs_lgl <- logical(length = length(renv_lock$Packages))
-    for (i in seq_along(renv_lock$Packages)) {
-        if (renv_lock$Packages[[i]]$Source == "Repository") {
-            repo_pkgs_lgl[i] <- TRUE
-        } else {
-            repo_pkgs_lgl[i] <- FALSE
-        }
-    }
-    git_pkgs <- NULL
-    local_r_pkgs <- NULL
-    # remotes package supports these types
-    # github (is assumed) gitlab,bitbucket, git, local, svn, url, version, cran, bioc.
-    # may need handling for ssh and any other support non https protocols?
-    if (any(!repo_pkgs_lgl)) {
-        for (x in renv_lock$Packages[!repo_pkgs_lgl]) {
-            if (x$RemoteType == "github") {
-                git_pkgs[[x$Package]] <- list(
-                    package_name = x$Package,
-                    repo_url = paste0(
-                        # RemoteHost is listed as api.github.com for some reason
-                        "https://github.com/", x$RemoteUser, "/",
-                        x$RemoteRepo
-                    ),
-                    commit = x$RemoteSha
-                )
-            #  this may work with other git remotes and possibly also bitbucket / svn needs checking
-            } else if (x$RemoteType == "gitlab") {
-                git_pkgs[[x$Package]] <- list (
-                    package_name = x$Package,
-                    repo_url = paste0(
-                        "https://", x$RemoteHost, "/", x$RemoteUser, "/",
-                        x$RemoteRepo
-                    ),
-                    commit = x$RemoteSha
-                )
+    if (method == "fast") {
+        repo_pkgs_lgl <- logical(length = length(renv_lock$Packages))
+        for (i in seq_along(renv_lock$Packages)) {
+            if (renv_lock$Packages[[i]]$Source == "Repository") {
+                repo_pkgs_lgl[i] <- TRUE
+            } else {
+                repo_pkgs_lgl[i] <- FALSE
             }
-            # as local_r_pkgs expects an archive not sure how to set type here..
-            #  else if (x$RemoteType == "local") else {
-            #      local_r_pkgs[[x$Package]] <- c(
-            #          package_name = paste0(x$RemoteUrl, "/", x$Package, ".tar.gz")
-            #      )
-            #  }
         }
-    }
-    rix_call <- call("rix",
-        r_ver = renv_lock$R$Version,
-        r_pkgs = names(renv_lock$Packages[repo_pkgs_lgl]),
-        git_pkgs = git_pkgs,
-        local_r_pkgs = local_r_pkgs
-    )
-    dots <- list(...)
-    for(arg in names(dots)) {
-        rix_call[[arg]] <- dots[[arg]]
-    }
+        git_pkgs <- NULL
+        # as local_r_pkgs expects an archive not sure how to set type here..
+        # local_r_pkgs <- NULL
+        if (any(!repo_pkgs_lgl)) {
+            for (x in renv_lock$Packages[!repo_pkgs_lgl]) {
+                git_pkgs[[x$Package]] <- renv_remote_pkgs(x)
+            }
+        }
+        rix_call <- call("rix",
+            r_ver = renv_lock$R$Version,
+            r_pkgs = names(renv_lock$Packages[repo_pkgs_lgl]),
+            git_pkgs = git_pkgs#,
+            #local_r_pkgs = local_r_pkgs
+        )
+        dots <- list(...)
+        for(arg in names(dots)) {
+            rix_call[[arg]] <- dots[[arg]]
+        }
 
-    if (return_rix_call) {
-        # print(rix_call)
-        # return(deparse(substitute(rix_call)))
-        return(rix_call)
+        if (return_rix_call) {
+            # print(rix_call)
+            return(rix_call)
+        }
+        eval(rix_call)
+    } else {
+        stop(
+            "'accurate' renv based environments with package version matching",
+            " is not yet implemented :("
+        )
     }
-    eval(rix_call)
 }
 
 #' renv_lock_r_ver
