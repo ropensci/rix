7c108be816013f4e6040c6b46eef394fb6f1a3ee
Richard J. Acton <richardjacton@pm.me>
2024-10-13 17:31:15 +0100

    add simple tests for `renv_read_lock()`

--- tests/testthat/test-renv_helpers.R
+++ tests/testthat/test-renv_helpers.R
@@ -1,1 +1,8 @@
+testthat::test_that("Testing `read_renv_lock()`", {
+    testthat::expect_error(read_renv_lock("nosuchfile"), "nosuchfile does not exist")
+    tmpf <- tempfile()
+    cat("not json", file = tmpf)
+    testthat::expect_error(read_renv_lock(tmpf), "Error reading renv\\.lock file")
+    unlink(tmpf)
 })
+
