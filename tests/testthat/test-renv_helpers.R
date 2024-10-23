753c30b4a15e40ed3cb7abd6b8a9c46ab3bf87d7
Richard J. Acton <richardjacton@pm.me>
2024-10-23 13:36:27 +0100

    nest renv_helper tests to keep example inputs out of the global namespace

--- tests/testthat/test-renv_helpers.R
+++ tests/testthat/test-renv_helpers.R
@@ -1,53 +1,18 @@
-testthat::test_that("Testing `read_renv_lock()`", {
-  testthat::expect_error(read_renv_lock("nosuchfile"), "nosuchfile does not exist")
-  tmpf <- tempfile()
-  cat("not json", file = tmpf)
-  testthat::expect_error(read_renv_lock(tmpf), "Error reading renv\\.lock file")
-  unlink(tmpf)
-})
+testthat::test_that("testing renv_helpers", {
+  testthat::expect_true(exists("renv2nix"))
+  # following as nested test pattern based on:
+  # https://rpahl.github.io/r-some-blog/posts/2024-10-07-nested-unit-tests-with-testthat/
 
-testthat::test_that("Testing `renv_remote_pkg()`", {
-  synthetic_renv_lock_example <- list(
-    githubpkg = list(
-      Package = "githubpkg",
-      RemoteType = "github",
-      RemoteUser = "user",
-      RemoteRepo = "repo",
-      RemoteSha = "yki8snny7wgpjolz5cq0bwxjshxdd0xv0mcyygoz",
-      RemoteHost = "api.github.com"
-    ),
-    gitlabpkg = list(
-      Package = "gitlabpkg",
-      RemoteType = "gitlab",
-      RemoteUser = "user",
-      RemoteRepo = "repo",
-      RemoteSha = "45p9megdp0i5230rtw1lisy6rquc466zb9yxn7eh",
-      RemoteHost = "gitlab.com"
-    )
-  )
+  # testthat::skip("skipping remaining renv_helpers tests...") # uncomment to skip subsequent tests
 
-  expected_git_pkg <- list(
-    githubpkg = list(
-      package_name = "githubpkg",
-      repo_url = "https://github.com/user/repo",
-      commit = "yki8snny7wgpjolz5cq0bwxjshxdd0xv0mcyygoz"
-    ),
-    gitlabpkg = list(
-      package_name = "gitlabpkg",
-      repo_url = "https://gitlab.com/user/repo",
-      commit = "45p9megdp0i5230rtw1lisy6rquc466zb9yxn7eh"
-    )
-  )
+  testthat::test_that("Testing `read_renv_lock()`", {
+    testthat::expect_error(read_renv_lock("nosuchfile"), "nosuchfile does not exist")
+    tmpf <- tempfile()
+    cat("not json", file = tmpf)
+    testthat::expect_error(read_renv_lock(tmpf), "Error reading renv\\.lock file")
+    unlink(tmpf)
+  })
 
-  testthat::expect_equal(
-    renv_remote_pkg(synthetic_renv_lock_example$githubpkg), expected_git_pkg$githubpkg
-  )
-  testthat::expect_equal(
-    renv_remote_pkg(synthetic_renv_lock_example$gitlabpkg), expected_git_pkg$gitlabpkg
-  )
-})
-
-testthat::test_that("Testing `renv2nix()`", {
   synthetic_renv_lock_example <- list(
     R = list(Version = "4.4.1"),
     Packages = list(
@@ -87,30 +52,47 @@ testthat::test_that("Testing `renv2nix()`", {
       )
     )
   )
-  tmpf <- tempfile()
-  jsonlite::write_json(synthetic_renv_lock_example, tmpf, auto_unbox = TRUE)
-  test_call <- call(
-    "rix",
-    r_ver = "4.4.1", r_pkgs = c("MASS", "R6"), git_pkgs = list(
-      githubpkg = list(
-        package_name = "githubpkg",
-        repo_url = "https://github.com/user/repo",
-        commit = "yki8snny7wgpjolz5cq0bwxjshxdd0xv0mcyygoz"
-      ),
-      gitlabpkg = list(
-        package_name = "gitlabpkg",
-        repo_url = "https://gitlab.com/user/repo",
-        commit = "45p9megdp0i5230rtw1lisy6rquc466zb9yxn7eh"
-      )
+
+  expected_git_pkg <- list(
+    githubpkg = list(
+      package_name = "githubpkg",
+      repo_url = "https://github.com/user/repo",
+      commit = "yki8snny7wgpjolz5cq0bwxjshxdd0xv0mcyygoz"
+    ),
+    gitlabpkg = list(
+      package_name = "gitlabpkg",
+      repo_url = "https://gitlab.com/user/repo",
+      commit = "45p9megdp0i5230rtw1lisy6rquc466zb9yxn7eh"
     )
   )
-  testthat::expect_equal(test_call, renv2nix(tmpf, return_rix_call = TRUE))
-  unlink(tmpf)
+
+  testthat::test_that("Testing `renv_remote_pkg()`", {
+    testthat::expect_equal(
+      renv_remote_pkgs(synthetic_renv_lock_example$Packages[c("githubpkg","gitlabpkg")]),
+      expected_git_pkg
+    )
+    testthat::expect_error(
+      renv_remote_pkgs(synthetic_renv_lock_example$Packages),
+      "Not a package installed from a remote outside of the main package repositories"
+    )
+  })
+
+  testthat::test_that("Testing `renv2nix()`", {
+    tmpf <- tempfile()
+    jsonlite::write_json(synthetic_renv_lock_example, tmpf, auto_unbox = TRUE)
+    test_call <- call(
+      "rix", r_ver = "4.4.1", r_pkgs = c("MASS", "R6"), git_pkgs = expected_git_pkg
+    )
+    testthat::expect_equal(test_call, renv2nix(tmpf, return_rix_call = TRUE))
+    unlink(tmpf)
+  })
+
+  testthat::test_that("Testing `renv_lock_r_ver()`", {
+    tmpf <- tempfile()
+    jsonlite::write_json(list(R = list(Version = "4.4.1")), tmpf, auto_unbox = TRUE)
+    testthat::expect_equal(renv_lock_r_ver(renv_lock_path = tmpf), "4.4.1")
+    unlink(tmpf)
+  })
+
 })
 
-testthat::test_that("Testing `renv_lock_r_ver()`", {
-  tmpf <- tempfile()
-  jsonlite::write_json(list(R = list(Version = "4.4.1")), tmpf, auto_unbox = TRUE)
-  testthat::expect_equal(renv_lock_r_ver(renv_lock_path = tmpf), "4.4.1")
-  unlink(tmpf)
-})
