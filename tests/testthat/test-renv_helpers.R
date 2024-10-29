c0c7b3ccdd2f6e68931d5c080a6e25095ddce9a1
Richard J. Acton <richardjacton@pm.me>
2024-10-29 10:48:14 +0000

    test sample renv.lock files can be read

--- tests/testthat/test-renv_helpers.R
+++ tests/testthat/test-renv_helpers.R
@@ -5,12 +5,18 @@ testthat::test_that("testing renv_helpers", {
 
   # testthat::skip("skipping remaining renv_helpers tests...") # uncomment to skip subsequent tests
 
+  renv_sample_dir <- paste0(testthat::test_path(), "/testdata/renv-samples")
+  renv_sample_files <- list.files(renv_sample_dir, pattern = "*.lock", full.names = TRUE)
+
   testthat::test_that("Testing `read_renv_lock()`", {
     testthat::expect_error(read_renv_lock("nosuchfile"), "nosuchfile does not exist")
     tmpf <- tempfile()
     cat("not json", file = tmpf)
     testthat::expect_error(read_renv_lock(tmpf), "Error reading renv\\.lock file")
     unlink(tmpf)
+    for (file in renv_sample_files) {
+      testthat::expect_type(read_renv_lock(file), "list")
+    }
   })
 
   synthetic_renv_lock_example <- list(
