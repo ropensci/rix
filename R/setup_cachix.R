600b5e3999dff64b20e9f96ca10c782971984a0e
Bruno Rodrigues <bruno@brodrigues.co>
2024-12-10 21:31:46 +0100

    added onload message

--- R/setup_cachix.R
+++ R/setup_cachix.R
@@ -14,20 +14,13 @@
 setup_cachix <- function(nix_conf_path = "~/.config/nix") {
   nix_conf_file <- file.path(nix_conf_path, "nix.conf")
 
-  # Test for existence and size instead of only existence,
-  # as an active file connection makes the file exist, but is empty
-  nix_conf_exists <- file.exists(nix_conf_file) &&
-    `!=`(file.size(nix_conf_file), 0L)
-
-  if (!nix_conf_exists) {
+  if (!nix_conf_exists(nix_conf_file)) {
     stop("~/.config/nix/nix.conf does not exist, did you install Nix?")
   }
 
   nix_conf_content <- readLines(con = nix_conf_file)
 
-  if (
-    (grepl("rstats-on-nix", nix_conf_content[1]))
-  ) {
+  if (is_cachix_configured(nix_conf_content)) {
     stop("rstats-on-nix cache already configured!")
   } else {
     if (identical(Sys.getenv("TESTTHAT"), "true")) {
@@ -38,24 +31,41 @@ setup_cachix <- function(nix_conf_path = "~/.config/nix") {
       file.copy(from = nix_conf_file, to = nix_conf_backup)
     }
 
-
-    nix_conf_content[1] <- paste0(
+    substituter_line <- grep("substituters", nix_conf_content)
+    nix_conf_content[substituter_line] <- paste0(
       append(
-        nix_conf_content[1],
+        nix_conf_content[substituter_line],
         "https://rstats-on-nix.cachix.org"
       ),
       collapse = " "
     )
 
-    nix_conf_content[2] <- paste0(
+    key_line <- grep("trusted-public-keys", nix_conf_content)
+    nix_conf_content[key_line] <- paste0(
       append(
-        nix_conf_content[2],
+        nix_conf_content[key_line],
         "rstats-on-nix.cachix.org-1:mzXrOo5XyDwy6MWSY0v8XYXTeYFSg7QSfv9Vq3Xvwyk="
       ),
       collapse = " "
     )
 
     writeLines(enc2utf8(nix_conf_content), nix_conf_file, useBytes = TRUE)
-    nix_conf_file
+    message("Added rstats-on-nix as a substituter to ", nix_conf_file)
+    invisible(nix_conf_file)
   }
+}
+
+
+#' @noRd
+nix_conf_exists <- function(nix_conf_file){
+                                        # Test for existence and size instead of only existence,
+                                        # as an active file connection makes the file exist, but is empty
+  file.exists(nix_conf_file) &&
+    `!=`(file.size(nix_conf_file), 0L)
+}
+
+#' @noRd
+is_cachix_configured <- function(nix_conf_content){
+  substituter_line <- grep("substituters", nix_conf_content)
+  (grepl("rstats-on-nix", nix_conf_content[substituter_line]))
 }
--- R/zzz.R
+++ R/zzz.R
@@ -2,3 +2,21 @@
 #' @noRd
 #' @importFrom utils data
 utils::globalVariables(c("sysdata", "is_internet_down", "type", "package"))
+
+#' @noRd
+.onLoad <- function(libname, pkgname) {
+  if (!nix_build_installed()) {
+    message("Nix doesn't seem to be installed on this system.
+You can still generate Nix expressions, but you won't be able to build them.")
+  }
+
+  nix_conf_path <- "~/.config/nix/nix.conf"
+
+  if (nix_conf_exists(nix_conf_path) &&
+      !is_cachix_configured(readLines(nix_conf_path))) {
+    message("To speed up the build process of your development environments,
+don't forget to run `setup_cachix()`!")
+  }
+
+}
+
