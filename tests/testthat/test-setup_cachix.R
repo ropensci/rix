91c069bbf02ceee1d68e8495c37db67fbbbd0c64
Bruno Rodrigues <bruno@brodrigues.co>
2024-12-31 18:43:06 +0100

    added test for when nix is not available for setup_cachix()

--- tests/testthat/test-setup_cachix.R
+++ tests/testthat/test-setup_cachix.R
@@ -1,10 +1,19 @@
 testthat::test_that("setup_cachix(): error if already configured", {
+  skip_if_not(nix_shell_available())
   testthat::expect_error(
     setup_cachix(nix_conf_path = "testdata/nix_conf_samples/nix_already/"), "already configured"
   )
 })
 
+testthat::test_that("setup_cachix(): error if no nix", {
+  skip_if_not(!nix_shell_available())
+  testthat::expect_error(
+    setup_cachix(nix_conf_path = "testdata/nix_conf_samples/nix_already/"), "no need"
+  )
+})
+
 testthat::test_that("setup_cachix(): configure if not already", {
+  skip_if_not(nix_shell_available())
   testthat::expect_snapshot_file(
     path = setup_cachix(nix_conf_path = "testdata/nix_conf_samples/nix_not_yet/"),
     name = "nix.conf"
