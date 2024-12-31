7fda634d434da24cfbbe0f2dbcf659bd5a563bf9
Bruno Rodrigues <bruno@brodrigues.co>
2024-12-31 20:08:52 +0100

    onLoad -> onAttach and message -> packageStartupMessage

--- R/zzz.R
+++ R/zzz.R
@@ -4,16 +4,16 @@
 utils::globalVariables(c("sysdata", "is_internet_down", "type", "package"))
 
 #' @noRd
-.onLoad <- function(libname, pkgname) {
+.onAttach <- function(libname, pkgname) {
   if (!nix_build_installed()) {
-    message("Nix doesn't seem to be installed on this system.
+    packageStartupMessage("Nix doesn't seem to be installed on this system.
 You can still generate Nix expressions, but you won't be able to build them.")
   }
 
   nix_conf_path <- "~/.config/nix/nix.conf"
 
   if (nix_conf_exists(nix_conf_path) && !is_cachix_configured(readLines(nix_conf_path))) {
-    message("To speed up the build process of your development environments,
+    packageStartupMessage("To speed up the build process of your development environments,
 configure the rstats-on-nix binary repository! Read the `Getting started`
 vignette for instructions see
 https://docs.ropensci.org/rix/articles/a-getting-started.html#installing-and-configuring-nix
@@ -21,7 +21,7 @@ https://docs.ropensci.org/rix/articles/a-getting-started.html#installing-and-con
   }
 
   if (!nix_conf_exists(nix_conf_path) && nix_build_installed()) {
-    message("To speed up the build process of your development environments,
+    packageStartupMessage("To speed up the build process of your development environments,
 configure the rstats-on-nix binary repository! Read the `Getting started`
 vignette for instructions see
 https://docs.ropensci.org/rix/articles/a-getting-started.html#installing-and-configuring-nix
