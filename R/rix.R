8e375a936bc590aa41cda2f9e59f01baffc78130
mischko <mhem@mailbox.org>
2025-01-05 14:24:09 +0100

    add comments back

--- R/rix.R
+++ R/rix.R
@@ -290,7 +290,11 @@ for more details."
         error = function(e) git_pkgs$package_name
       )
     }
-
+    # CRAN archive pkgs are written as "AER@123"
+    # so we need to split at the '@' character and then
+    # walk through the list to grab the first element
+    # which will be the name of the package
+    pkgs <- strsplit(cran_pkgs$archive_pkg, split = "@")
     cran_archive_names <- if (!is.null(cran_pkgs$archive_pkgs)) {
       pkgs <- strsplit(cran_pkgs$archive_pkgs, split = "@")
       sapply(pkgs, function(x) x[[1]])
