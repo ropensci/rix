51f30c2334a3f6b28e96df0401359df02237852e
Bruno Rodrigues <bruno@brodrigues.co>
2025-02-17 18:36:05 +0100

    remove error to make tests pass

--- tests/testthat/test-available_r.R
+++ tests/testthat/test-available_r.R
@@ -3,7 +3,7 @@ testthat::test_that("available_r lists all available r versions", {
     available_r(),
     c(
       "bleeding-edge", "frozen-edge", "r-devel", "bioc-devel",
-      "r-devel-bioc-devel", "latest-upstream", "hu",
+      "r-devel-bioc-devel", "latest-upstream", 
       "3.5.3", "3.6.0", "3.6.1", "3.6.2", "3.6.3",
       "4.0.0", "4.0.1", "4.0.2", "4.0.3", "4.0.4", "4.0.5",
       "4.1.0", "4.1.1", "4.1.2", "4.1.3",
