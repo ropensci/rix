46a6ddd602255f08a7b3e50dbe1e95ebc97bd794
mischko <mhem@mailbox.org>
2025-02-20 23:47:15 +0100

    improve code logic

--- R/fetchers.R
+++ R/fetchers.R
@@ -652,12 +652,10 @@ resolve_package_commit <- function(remote_pkg_name_and_ref, date, remotes, ...) 
     }
   }
 
-  cache_key <- if (length(remote_pkg_name_and_ref) == 2) {
-    paste0(pkg_name, "@", remote_pkg_name_and_ref[[2]])
-  }
   # If ref (commit hash) is provided, use it
-  commit <- if (length(remote_pkg_name_and_ref) == 2) {
-    remote_pkg_name_and_ref[[2]]
+  if (length(remote_pkg_name_and_ref) == 2) {
+    cache_key <- paste0(pkg_name, "@", remote_pkg_name_and_ref[[2]])
+    commit <- remote_pkg_name_and_ref[[2]]
   } else if (length(remote_pkg_name_and_ref) == 1) {
     # For packages without ref, try to find closest one by date
     # fallback to HEAD if API fails
