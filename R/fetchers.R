9253aba737fcbd8ddaa3c65508c501145b42bf92
Bruno Rodrigues <brodriguesco@protonmail.com>
2025-02-27 14:16:19 +0100

    removing trailing /

--- R/fetchers.R
+++ R/fetchers.R
@@ -7,7 +7,7 @@
 #' @noRd
 fetchgit <- function(git_pkg) {
   package_name <- git_pkg$package_name
-  repo_url <- git_pkg$repo_url
+  repo_url <- sub("/$", "", git_pkg$repo_url)
   commit <- unlist(git_pkg$commit)
 
   output <- get_sri_hash_deps(repo_url, commit)
