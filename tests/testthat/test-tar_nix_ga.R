9ca22520d0d9088df55d049414f500d0a81fb23e
Bruno Rodrigues <bruno@brodrigues.co>
2025-02-28 15:16:43 +0100

    remove announcing function

--- tests/testthat/test-tar_nix_ga.R
+++ tests/testthat/test-tar_nix_ga.R
@@ -1,6 +1,4 @@
 testthat::test_that("Snapshot test of tar_nix_ga()", {
-  testthat::announce_snapshot_file("tar_nix_ga/run-pipeline.yaml")
-
   testthat::expect_snapshot_file(
     path = tar_nix_ga(),
     name = "run-pipeline.yaml"
