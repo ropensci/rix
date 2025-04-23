9c65a53eda67f687bec7e15db538291a2c884bd3
Bruno Rodrigues <bruno@brodrigues.co>
2025-04-23 20:13:56 +0200

    tests are ok

--- tests/testthat/_snaps/rix-python/python_default.nix
+++ tests/testthat/_snaps/rix-python/python_default.nix
@@ -5,7 +5,8 @@ let
   rpkgs = builtins.attrValues {
     inherit (pkgs.rPackages) 
       dplyr
-      janitor;
+      janitor
+      reticulate;
   };
   
   tex = (pkgs.texlive.combine {
@@ -34,11 +35,12 @@ let
   shell = pkgs.mkShell {
     LOCALE_ARCHIVE = if pkgs.system == "x86_64-linux" then "${pkgs.glibcLocales}/lib/locale/locale-archive" else "";
     LANG = "en_US.UTF-8";
-   LC_ALL = "en_US.UTF-8";
-   LC_TIME = "en_US.UTF-8";
-   LC_MONETARY = "en_US.UTF-8";
-   LC_PAPER = "en_US.UTF-8";
-   LC_MEASUREMENT = "en_US.UTF-8";
+    LC_ALL = "en_US.UTF-8";
+    LC_TIME = "en_US.UTF-8";
+    LC_MONETARY = "en_US.UTF-8";
+    LC_PAPER = "en_US.UTF-8";
+    LC_MEASUREMENT = "en_US.UTF-8";
+    RETICULATE_PYTHON = '${pkgs.python312}/bin/python';
 
     buildInputs = [  rpkgs tex pyconf system_packages   ];
     
--- tests/testthat/_snaps/rix/no_quarto_default.nix
+++ tests/testthat/_snaps/rix/no_quarto_default.nix
@@ -17,12 +17,12 @@ let
   shell = pkgs.mkShell {
     LOCALE_ARCHIVE = if pkgs.system == "x86_64-linux" then "${pkgs.glibcLocales}/lib/locale/locale-archive" else "";
     LANG = "en_US.UTF-8";
-   LC_ALL = "en_US.UTF-8";
-   LC_TIME = "en_US.UTF-8";
-   LC_MONETARY = "en_US.UTF-8";
-   LC_PAPER = "en_US.UTF-8";
-   LC_MEASUREMENT = "en_US.UTF-8";
-
+    LC_ALL = "en_US.UTF-8";
+    LC_TIME = "en_US.UTF-8";
+    LC_MONETARY = "en_US.UTF-8";
+    LC_PAPER = "en_US.UTF-8";
+    LC_MEASUREMENT = "en_US.UTF-8";
+    
     buildInputs = [  rpkgs   system_packages   ];
     
   }; 
